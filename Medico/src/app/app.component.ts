587dc79ffb21b8179a781bd770d2b81490295022
GianlucaSagheddu <gianlucasagheddu@gmail.com>
2019-04-30 08:41:09 +0000

    DB

--- Medico/src/app/app.component.ts
+++ Medico/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Visita } from './visita/visita.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Medico';
+  articles: Visita[] = [];   // <-- component property
+  data: Object ={};
+  loading: boolean;
+  o :Observable<Visita[]>;
+
+  constructor(public http: HttpClient){
+    this.makeTypedRequest();
+
+
+  }
+
+  makeTypedRequest(): void {
+    this.o = this.http.get<Article[]>('https://jsonplaceholder.typicode.com/posts');
+    this.o.subscribe(data => {
+        for(var i = 0; i < data.length; i++){
+          this.articles.push(new Article(data[i].userId, data[i].id, data[i].title, data[i].body));
+
+          }
+
+      });
+
+   }
+
 }
