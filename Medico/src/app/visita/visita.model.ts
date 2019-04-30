7108b81b7c4fad025cba010bb8a722d660ddc557
GianlucaSagheddu <gianlucasagheddu@gmail.com>
2019-04-30 08:57:48 +0000

    DB

--- Medico/src/app/visita/visita.model.ts
+++ Medico/src/app/visita/visita.model.ts
@@ -1,3 +1,17 @@
 export class Visita {
+  Nome: string;
+  Cognome: string;
+  Indirizzo: string;
+  Tel: string;
+  Email: string;
+  Data: Date;
 
+
+  constructor(Nome: string, Cognome: string, Indirizzo: string, Tel: string, Email: string) {
+    this.Nome = Nome;
+    this.Cognome = Cognome;
+    this.Indirizzo = Indirizzo;
+    this.Tel = Tel;
+    this.Email = Email;
+  }
 }
